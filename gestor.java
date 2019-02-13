1c1d7d7f61bb3845b7b4bec10045ea63506d836d
Areias <paulocarlaoareias@gmail.com>
2019-02-13 11:34:09 +0000

    ajeitei o Registar Concessionario

--- gestor.java
+++ gestor.java
@@ -24,27 +24,17 @@ public class gestor {
 	
 	public static void Criar() {
 		
-		int x, i=1;
 		concessionario c1 = new concessionario();
 		Scanner read = new Scanner(System.in);
 		
 		System.out.print("Distrito: ");
 		c1.setDistrito(read.nextLine());
-		System.out.print("Quantos carros para test drive tem disponivel?");
-		x=read.nextInt();
 		
-		Next carros[]= new Next[5];
-		
-		carros[0].setPcarro();
-		
-		while (i<5) {
-			
-		System.out.print("Potencia do Carro "+(i+1)+": ");
-		
+		System.out.println("1) "+c1.getDistrito());
 		}
-		
-	}
 	
+	
+			
 	
 	
 	
@@ -60,13 +50,15 @@ public class gestor {
 			
 			switch (opc) {
 			case 1:
+				Criar();
 				
 				break;
 			case 2:
 				break;
 			case 0:
 				break;
-			default:	
+			default:		
+				
 				System.out.println( " " ) ;
                 System.out.println( "Erro! Opção escolhida não existe!" ) ;  
                 System.out.println( " " ) ;
