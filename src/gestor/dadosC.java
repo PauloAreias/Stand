ed546319920b802adf22e93ea33158c82956dc33
Carlos <carlosjpcastro.000@gmail.com>
2019-02-20 11:54:52 +0000

    fix

--- src/gestor/dadosC.java
+++ src/gestor/dadosC.java
@@ -1,4 +1,4 @@
-package gestor;
+package src.gestor;
 
 public class dadosC {
 
--- src/gestor/gestor.java
+++ src/gestor/gestor.java
@@ -1,53 +1,9 @@
-package gestor;
+package src.gestor;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class gestor {
-	
-	public static void menu() {
-		
-		System.out.println( "========== GESTOR DE AUTOMOVEIS ==========" ) ;  
-        System.out.println( "Menu: " ) ;  
-        System.out.println( "1 - Registar Concessionario" ) ;  
-        System.out.println( "2 - Registar Cliente " ) ;  
-        System.out.println( "3 - Registar Carro de demonstração " ) ; 
-        System.out.println( "4 - Consultar Concessionario" ) ;  
-        System.out.println( "5 - Consultar Cliente " ) ;  
-        System.out.println( "6 - Consultar Carros de demonstração");
-        System.out.println( "0 - Sair" ) ; 
-       
-        
-        
-        System.out.print( "Digite a opção pretendida: " );
-		
-	}
-	
-	public static void Criar() {
-		
-		String n ;
-		concessionario c1 = new concessionario();
-		Scanner read = new Scanner(System.in);
-		ArrayList<String> list = new ArrayList<>();
-		
-		System.out.print("Distrito: ");
-		n= read.nextLine();
-		c1.setDistrito(n);
-		
-		list.add(n);
-		
-		
-		
-		System.out.println("1) "+ list);
-		
-		}
-	
-	
-			
-	
-	
-	
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		
 		int opc;
 		Scanner read = new Scanner(System.in);
@@ -77,5 +33,41 @@ public class gestor {
 		}while(opc!=0);
 		
 	}
+	
+	public static void menu() {
+		
+		System.out.println( "========== GESTOR DE AUTOMOVEIS ==========" ) ;  
+        System.out.println( "Menu: " ) ;  
+        System.out.println( "1 - Registar Concessionario" ) ;  
+        System.out.println( "2 - Registar Cliente " ) ;  
+        System.out.println( "3 - Registar Carro de demonstração " ) ; 
+        System.out.println( "4 - Consultar Concessionario" ) ;  
+        System.out.println( "5 - Consultar Cliente " ) ;  
+        System.out.println( "6 - Consultar Carros de demonstração");
+        System.out.println( "0 - Sair" ) ; 
+       
+        
+        
+        System.out.print( "Digite a opção pretendida: " );
+		
+	}
+	
+	public static void Criar() {
+		
+		concessionario c1 = new concessionario();
+		Scanner read = new Scanner(System.in);
+		
+		System.out.print("Distrito: ");
+		c1.setDistrito(read.nextLine());
+		
+		System.out.println("1) "+c1.getDistrito());
+		}
+	
+	
+			
+	
+	
+	
+	
 
 }
